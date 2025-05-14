218ba302cd2b6566ccfe919c270c39c04ceb70b6
SanjayDutta-code <dutta07sanjay2003@gmail.com>
2025-05-14 17:17:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// Detect button clicks
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML.toLowerCase();
+
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+// Detect keyboard key presses
+document.addEventListener("keydown", function (event) {
+  var keyPressed = event.key.toLowerCase(); // normalize to lowercase
+  makeSound(keyPressed);
+  buttonAnimation(keyPressed);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+    default:
+      console.log("Unmapped key: " + key);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  if (activeButton) {
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+      activeButton.classList.remove("pressed");
+    }, 100);
+  }
+}
